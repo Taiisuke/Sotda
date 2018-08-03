6cc900abffd253249fc5949dac83e18d6de2d193
Ferna <Ferna@Nando>
2018-08-02 19:59:20 -0500

    MIKE

---
+++ src/com/utez/modelo/carrera/BeanCarrera.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utez.modelo.carrera;
+
+/**
+ *
+ * @author Ferna
+ */
+public class BeanCarrera {
+    private int idCarrera;
+    private String nombreCarrera;
+
+    public int getIdCarrera() {
+        return idCarrera;
+    }
+
+    public void setIdCarrera(int idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+
+    public String getNombreCarrera() {
+        return nombreCarrera;
+    }
+
+    public void setNombreCarrera(String nombreCarrera) {
+        this.nombreCarrera = nombreCarrera;
+    }
+
+    public BeanCarrera(int idCarrera, String nombreCarrera) {
+        this.idCarrera = idCarrera;
+        this.nombreCarrera = nombreCarrera;
+    }
+    
+}
