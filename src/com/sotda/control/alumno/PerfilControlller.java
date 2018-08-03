2ca39ce83f623b06eb1c46c93fe0bd449a7ddb75
Ferna <Ferna@Nando>
2018-08-02 20:00:40 -0500

    MIKE

--- src/com/sotda/control/alumno/PerfilControlller.java
+++ src/com/sotda/control/alumno/PerfilControlller.java
@@ -19,12 +19,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /**
@@ -74,7 +71,6 @@ public class PerfilControlller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-
        consultarDatos();
     }
 
@@ -196,12 +192,16 @@ public class PerfilControlller implements Initializable {
                 verticalBox.getChildren().add(0, parent1);
     }
 
-    @FXML
-    private void registrar(ActionEvent event) {
-    }
 
     @FXML
     private void cancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void modificar(ActionEvent event) {
+    }
+    public void consultarMasDatos(){
+        DaoAlumno daoAlumno = new DaoAlumno();
     }
 
 }
